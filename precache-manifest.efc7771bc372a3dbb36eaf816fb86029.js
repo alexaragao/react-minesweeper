588b2f3d60f9760c5e22efe89394988bf8313acb
Alexandre <alexr.aragao@gmail.com>
2020-08-17 21:44:36 -0300

    Updates

---
+++ precache-manifest.efc7771bc372a3dbb36eaf816fb86029.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d121aa4badbb23c1d15d1c4a22f74e",
+    "url": "/react-minesweeper/index.html"
+  },
+  {
+    "revision": "2ff0de717cd19275bc4f",
+    "url": "/react-minesweeper/static/css/main.2334f762.chunk.css"
+  },
+  {
+    "revision": "36b7a2a8d9de2fba1607",
+    "url": "/react-minesweeper/static/js/2.9a98c704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-minesweeper/static/js/2.9a98c704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff0de717cd19275bc4f",
+    "url": "/react-minesweeper/static/js/main.e7666520.chunk.js"
+  },
+  {
+    "revision": "3641de6aa71eef7e8bd1",
+    "url": "/react-minesweeper/static/js/runtime-main.4a604056.js"
+  },
+  {
+    "revision": "f9ca5a946768427288969b0ac31499ff",
+    "url": "/react-minesweeper/static/media/bomb.f9ca5a94.svg"
+  },
+  {
+    "revision": "35d9cc9f161e9cf159c14e3e35d294cd",
+    "url": "/react-minesweeper/static/media/clock.35d9cc9f.svg"
+  },
+  {
+    "revision": "d69e7f585bcb22b6121852052eb5b296",
+    "url": "/react-minesweeper/static/media/flag.d69e7f58.svg"
+  },
+  {
+    "revision": "34b0645e48f3bfa1029338ccbaf2c700",
+    "url": "/react-minesweeper/static/media/unknow.34b0645e.svg"
+  }
+]);
